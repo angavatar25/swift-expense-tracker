58308b1c639e063ac42dbd86161836523952e64f
Ananda Afryan Gemilang <anandalark007@gmail.com>
2024-03-19 10:02:06 +0700

    Initial Commit

---
+++ Exspense Tracker/Exspense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exspense_TrackerApp.swift
+//  Exspense Tracker
+//
+//  Created by Ananda Afryan on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Exspense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
